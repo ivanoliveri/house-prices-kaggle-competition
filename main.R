068052e74511aa06d01153c21b024da693c23222
Ivan Oliveri <ipoliveri@gmail.com>
2017-02-09 00:10:45 -0300

    Added Outliers Filtering. Best Position so far!

--- main.R
+++ main.R
@@ -6,7 +6,9 @@ library(Metrics)
 
 library(ade4)
 
-str.workingPath <- "C:/Users/user/Documents/NuevaVersionHouseCompetition/"
+library(caret)
+
+str.workingPath <- "C:/Users/Ivan/Documents/GitHub/house-prices-kaggle-competition/"
 
 str.trainingFilename <- "train.csv"
 
@@ -125,6 +127,35 @@ for(str.columnName in vec.columnsNamesToRemove){
 
 df.allData <- data.frame(df.allData, df.newFactor)
 
+vec.columnsWithNearToZeroVariance <- c("MSZoning.C..all.","MSZoning.RH","Street.Grvl","LotShape.IR2","LotShape.IR3","LandContour.Low",
+                                       "Utilities.NoSeWa","LotConfig.FR3","LandSlope.Sev","Neighborhood.Blmngtn","Neighborhood.Blueste",
+                                       "Neighborhood.BrDale","Neighborhood.ClearCr","Neighborhood.MeadowV","Neighborhood.NoRidge",
+                                       "Neighborhood.NPkVill","Neighborhood.StoneBr","Neighborhood.SWISU","Neighborhood.Timber",
+                                       "Neighborhood.Veenker","Condition1.Artery","Condition1.PosA","Condition1.PosN","Condition1.RRAe",
+                                       "Condition1.RRAn","Condition1.RRNe","Condition1.RRNn","Condition2.Artery","Condition2.Feedr",
+                                       "Condition2.PosA", "Condition2.PosN","BldgType.2fmCon", "BldgType.Twnhs","HouseStyle.1.5Unf",
+                                       "HouseStyle.2.5Unf","HouseStyle.SFoyer","RoofStyle.Flat","RoofStyle.Gambrel","RoofStyle.Mansard",
+                                       "RoofStyle.Shed","RoofMatl.ClyTile","RoofMatl.CompShg","RoofMatl.Tar.Grv","RoofMatl.WdShake",
+                                       "RoofMatl.WdShngl","Exterior1st.AsbShng","Exterior1st.AsphShn","Exterior1st.BrkComm","Exterior1st.CBlock",
+                                       "Exterior1st.Stucco","Exterior1st.WdShing","Exterior2nd.AsbShng","Exterior2nd.AsphShn","Exterior2nd.Brk.Cmn",
+                                       "Exterior2nd.BrkFace","Exterior2nd.CBlock","Exterior2nd.ImStucc","Exterior2nd.Stone","Exterior2nd.Stucco",
+                                       "MasVnrType.BrkCmn","ExterCond.Ex","ExterCond.Fa","ExterCond.Po","Foundation.Slab","Foundation.Stone",
+                                       "Foundation.Wood","BsmtCond.Po","BsmtFinType2.ALQ","BsmtFinType2.BLQ","BsmtFinType2.GLQ","Heating.GasW",
+                                       "Heating.Grav","Heating.Wall","Electrical.FuseF","Electrical.FuseP","Functional.Maj1","Functional.Maj2",
+                                       "Functional.Min1","Functional.Min2","Functional.Mod","Functional.Sev","FireplaceQu.Ex","GarageType.Basment",
+                                       "GarageType.CarPort","GarageCond.Ex","GarageCond.Fa","GarageCond.Gd","GarageCond.Po","PavedDrive.P",
+                                       "SaleType.COD","SaleType.Con","SaleType.ConLD","SaleType.ConLI","SaleType.ConLw","SaleType.CWD",
+                                       "SaleType.Oth","SaleCondition.AdjLand","SaleCondition.Alloca","SaleCondition.Family")
+
+for(str.columnToRemove in vec.columnsWithNearToZeroVariance){
+  
+  
+  int.columnPositionToRemove <- which(str.columnToRemove == names(df.allData))
+  
+  df.allData <- df.allData[,-int.columnPositionToRemove]
+  
+}
+
 #Split into Training and Testing
 
 df.newTrain <- df.allData[1:1460,]
@@ -133,15 +164,71 @@ df.newTrain <- data.frame(df.newTrain,vec.transformedResponse)
   
 df.newTest <- df.allData[1461:2919,]
 
+#TRAIN MODELS
+
+firstLayer.control <- trainControl(method="cv", 
+                                   number = 5, 
+                                   allowParallel = TRUE,
+                                   verboseIter = TRUE)
+
+mars.grid <- expand.grid(degree = c(2,3,5))
+
+mars.model <- train(df.newTrain[,-length(df.newTrain)], 
+                    vec.transformedResponse,	
+                    method = 'gcvEarth', 
+                    trControl = firstLayer.control,
+                    tuneGrid = mars.grid,
+                    metric='RMSE')
+
 lm.model <- step(lm(vec.transformedResponse ~ ., data = df.newTrain))
 
-#TEST
-vec.predictions <- predict(lm.model, newdata = df.newTest)
+#Measure Training Before Outliers Filtering
 
-vec.finalPredictions <- 10^(vec.predictions) - 1
+vec.predictionsMARS <- 10^(predict(mars.model, newdata = df.newTrain)) - 1
 
-df.resultSet <- data.frame(Id = vec.idsTesting, 
-                           SalePrice = vec.finalPredictions)
+vec.predictionsLM <- 10^(predict(lm.model, newdata = df.newTrain)) - 1
 
-  write.csv(df.resultSet, file = paste(str.workingPath, 
-                                       "submission.csv", sep = ""), row.names=FALSE)
+vec.finalPredictions <-  (vec.predictionsMARS+vec.predictionsLM)/2
+
+print(paste("RMSE",rmse(log(df.newTrain$vec.transformedResponse,10),
+                        log(vec.finalPredictions,10))))
+
+#Measure Training After Outliers Filtering
+
+df.newTrainFiltered <- data.frame(df.newTrain, 
+                                  diff = (vec.finalPredictions-(10^(df.newTrain$vec.transformedResponse)-1))/
+                                          (10^(df.newTrain$vec.transformedResponse)-1))
+
+df.newTrainFiltered <- subset(df.newTrainFiltered, abs(diff)<0.4)
+
+df.newTrainFiltered <- df.newTrainFiltered[,-length(df.newTrainFiltered)]
+
+mars.model <- train(df.newTrainFiltered[,-length(df.newTrainFiltered)], 
+                    df.newTrainFiltered$vec.transformedResponse,	
+                    method = 'gcvEarth', 
+                    trControl = firstLayer.control,
+                    tuneGrid = mars.grid,
+                    metric='RMSE')
+
+lm.model <- step(lm(vec.transformedResponse ~ ., data = df.newTrainFiltered))
+
+vec.predictionsMARS <- 10^(predict(mars.model, newdata = df.newTrainFiltered)) - 1
+
+vec.predictionsLM <- 10^(predict(lm.model, newdata = df.newTrainFiltered)) - 1
+
+vec.finalPredictions <-  (vec.predictionsMARS+vec.predictionsLM)/2
+
+print(paste("RMSE",rmse(log(df.newTrainFiltered$vec.transformedResponse,10),
+     log(vec.finalPredictions,10))))
+
+#Measure Testing
+
+vec.predictionsMARS <- 10^(predict(mars.model, newdata = df.newTest)) - 1
+
+vec.predictionsLM <- 10^(predict(lm.model, newdata = df.newTest)) - 1
+
+vec.finalPredictions <-  (vec.predictionsMARS+vec.predictionsLM)/2
+
+df.resultSet <- data.frame(Id = vec.idsTesting, SalePrice = vec.finalPredictions)
+
+write.csv(df.resultSet, file = paste(str.workingPath, "submission.csv", sep = ""), row.names=FALSE)
